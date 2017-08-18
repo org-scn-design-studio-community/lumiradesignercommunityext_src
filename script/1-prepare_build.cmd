33cb9cc07081b1a90e08273cb3d9383ebf97a70f
Mike Howles <entmike@gmail.com>
2017-08-18 13:41:38 -0500

    Fix old links, fix ZTL for BYOD and Data Iterator component in Lumira Designer 2.0 SP2

--- script/1-prepare_build.cmd
+++ script/1-prepare_build.cmd
@@ -1,47 +1,47 @@
 set current=%cd%
 
 set module=org.scn.community.shared
-cd ..\src\%module%\script\
+cd ..\legacy_src\%module%\script\
 call build.cmd
 cd %current%
 
 set module=org.scn.community.utils
-cd ..\src\%module%\script\
+cd ..\legacy_src\%module%\script\
 call build.cmd
 cd %current%
 
 set module=org.scn.community.basics
-cd ..\src\%module%\script\
+cd ..\legacy_src\%module%\script\
 call build.cmd
 cd %current%
 
 set module=org.scn.community.databound
-cd ..\src\%module%\script\
+cd ..\legacy_src\%module%\script\
 call build.cmd
 cd %current%
 
 set module=org.scn.community.datasource
-cd ..\src\%module%\script\
+cd ..\legacy_src\%module%\script\
 call build.cmd
 cd %current%
 
 set module=org.scn.community.prototypes
-cd ..\src\%module%\script\
+cd ..\legacy_src\%module%\script\
 call build.cmd
 cd %current%
 
 set module=org.scn.community.geovis
-cd ..\src\%module%\script\
+cd ..\legacy_src\%module%\script\
 call build.cmd
 cd %current%
 
-copy ..\src\org.scn.community.basics\res\require_loader.js ..\src\org.scn.community.databound\res\require_loader.js /Y
-copy ..\src\org.scn.community.basics\res\require_loader.js ..\src\org.scn.community.utils\res\require_loader.js /Y
-copy ..\src\org.scn.community.basics\res\require_loader.js ..\src\org.scn.community.datasource\res\require_loader.js /Y
-copy ..\src\org.scn.community.basics\res\require_loader.js ..\src\org.scn.community.prototypes\res\require_loader.js /Y
-copy ..\src\org.scn.community.basics\res\require_loader.js ..\src\org.scn.community.shared\res\require_loader.js /Y
+copy ..\legacy_src\org.scn.community.basics\res\require_loader.js ..\legacy_src\org.scn.community.databound\res\require_loader.js /Y
+copy ..\legacy_src\org.scn.community.basics\res\require_loader.js ..\legacy_src\org.scn.community.utils\res\require_loader.js /Y
+copy ..\legacy_src\org.scn.community.basics\res\require_loader.js ..\legacy_src\org.scn.community.datasource\res\require_loader.js /Y
+copy ..\legacy_src\org.scn.community.basics\res\require_loader.js ..\legacy_src\org.scn.community.prototypes\res\require_loader.js /Y
+copy ..\legacy_src\org.scn.community.basics\res\require_loader.js ..\legacy_src\org.scn.community.shared\res\require_loader.js /Y
 
-copy ..\src\org.scn.community.basics\aps\*.* ..\src\org.scn.community.databound\aps\ /Y
-copy ..\src\org.scn.community.basics\aps\*.* ..\src\org.scn.community.utils\aps\ /Y
-copy ..\src\org.scn.community.basics\aps\*.* ..\src\org.scn.community.datasource\aps\ /Y
-copy ..\src\org.scn.community.basics\aps\*.* ..\src\org.scn.community.prototypes\aps\ /Y
+copy ..\legacy_src\org.scn.community.basics\aps\*.* ..\legacy_src\org.scn.community.databound\aps\ /Y
+copy ..\legacy_src\org.scn.community.basics\aps\*.* ..\legacy_src\org.scn.community.utils\aps\ /Y
+copy ..\legacy_src\org.scn.community.basics\aps\*.* ..\legacy_src\org.scn.community.datasource\aps\ /Y
+copy ..\legacy_src\org.scn.community.basics\aps\*.* ..\legacy_src\org.scn.community.prototypes\aps\ /Y

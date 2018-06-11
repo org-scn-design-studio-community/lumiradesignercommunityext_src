98e594321f1cf3b69c6e2314949adcc4e03a3605
Reiner Hille-Doering <reiner.hille-doering@sap.com>
2018-06-11 10:34:57 +0200

    Little improved error handling

--- src/org.scn.community.ui5view/res/ui5view.js
+++ src/org.scn.community.ui5view/res/ui5view.js
@@ -1,39 +1,40 @@
-define(["sap/designstudio/sdk/SDKModel"], function(SDKModel) {
+define([ "sap/designstudio/sdk/SDKModel" ], function(SDKModel) {
 	sap.m.Panel.extend("org.scn.community.ui5view.XMLView", {
-		initDesignStudio: function() {
+		initDesignStudio : function() {
 			// 
 		},
-		renderer: {},
-		setXml: function(xml) {
+		renderer : {},
+		setXml : function(xml) {
 			this._xml = xml;
+			this._view = null;
 		},
-		setData: function(json) {
+		setData : function(json) {
 			this._model = new SDKModel(json);
 			if (this._view) {
 				this._view.setModel(this._model);
 			}
 		},
-		setJs: function(js) {
+		setJs : function(js) {
 			try {
-				eval(js);				
-			}
-			catch(e) {
-				// Not sure what we could do
+				eval(js);
+			} catch (e) {
+				// ignore error during controller parsing
 			}
 		},
-		afterDesignStudioUpdate: function() {
-			if (!this._constructorCalled) {
+		afterDesignStudioUpdate : function() {
+			if (!this._view) {
+				this.destroyContent();
 				try {
-					this.destroyContent();
-				    this._view = new sap.ui.core.mvc.XMLView({viewContent: this._xml});
-				    this._view.placeAt(this);
-				} catch(e) {
-					
-				}
-				this._view.setModel(this._model);
-			}			
+					this._view = new sap.ui.core.mvc.XMLView({
+						viewContent : this._xml
+					});
+					this._view.setModel(this._model);
+				} catch (e) {
+					this._view = new sap.m.Text({text: e.toString()});
+				}				
+				this._view.placeAt(this);
+			}
 		}
 	});
-		
-});
 
+});
